69c466bd8dfc8b711d1551b88190fa3faf12f087
radhia01 <radhiarh2@gmail.com>
2024-11-15 16:26:57 -0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
+
 
 import './App.css'
 import SignUp from './pages/SignUp'
 import {Routes,Route} from "react-router-dom"
 import SignIn from './pages/SignIn'
-import Home from './pages/Home'
-import Navbar from './pages/Navbar'
+
 function App() {
   return (
-    <  >  
+    <div  >  
     
     <Routes >
-    
     <Route path="/" element={<SignIn/>}></Route>
-    <Route path="/signUp" element={<SignUp/>}></Route>
-    <Route path="*"  
-     element={
-  <div> <Navbar/>
-  <Routes>
-<Route path="/Home" element={<Home/>}></Route>
-</Routes>
-</div>
-}
-    > 
-    
-    
-    </Route>
-       
-  </Routes></>
-    
-  )
+    <Route path="/signUp" element={<SignUp/>}></Route> 
+  </Routes>
+  </div>)
 }
 
 export default App
